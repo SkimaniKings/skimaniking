a4c140f3c255284e3c0a375a4e8f0b28508b397b
skimanikings <kimanisimon856@gmail.com>
2020-03-25 00:12:26 +0300

    created my portfolio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var typed = new Typed('.type', {
+    strings: [
+        'Simon is a  Front-End Developer.',
+        'Also a Back-End Developer.',
+        'Making Simon a Full-Stack Developer.',
+ 
+        'And not forgeting he is also an Entrepreneur.',
+        
+    ],
+    typeSpeed:60,
+    backSpeed:60,
+    loop: true 
+  });
